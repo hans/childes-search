5ad07b079c279197f943016a4539a06a561fc8e3
Jon Gauthier <jon@gauthiers.net>
2019-08-22 10:26:49 -0400

    assume pre-parsed Chatter XML output is available, just run convert_conllu

--- 1_preprocess.sh
+++ 1_preprocess.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -e
 
-find . -name "*.cha" | \
-  xargs -I {} bash -c 'fn=`dirname {} | sed 's/[/]/_/g' | sed 's/^\._//g'`; fn2=`basename {}`; fn3="${fn}_${fn2}"; echo $fn3; java -cp ~/Downloads/chatter.jar org.talkbank.chatter.App {} > $fn3.xml'
-for x in *.xml; do python ../convert_conllu.py . $x > $x.conllu; done
+# find . -name "*.cha" | \
+#   xargs -I {} bash -c 'fn=`dirname {} | sed 's/[/]/_/g' | sed 's/^\._//g'`; fn2=`basename {}`; fn3="${fn}_${fn2}"; echo $fn3; java -cp chatter.jar org.talkbank.chatter.App {} > $fn3.xml'
+find . -name "*.xml" | xargs -I {} -n 1 bash -c "bn=\$(basename {}); echo \$bn && cp {} \$bn && python convert_conllu.py {} --speaker MOT > \$bn.conllu"
